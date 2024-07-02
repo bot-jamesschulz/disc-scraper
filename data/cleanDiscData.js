c7fa2efbfc6dbf5f3413f87c3a506066d8c21cee
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-07-01 22:54:35 -0700

    Added manufacturer list

--- data/cleanDiscData.js
+++ data/cleanDiscData.js
@@ -31,4 +31,30 @@ async function processDiscs() {
   }
 }
 
-processDiscs();
+async function getManufacturers() {
+  try {
+    // Read the contents of discsDefault.json
+    const data = await fs.readFile('./data/discs.json', 'utf-8');
+    const manufacturers = new Set()
+    
+    // Parse the JSON data
+    const discs = JSON.parse(data);
+    
+    // Perform any operations on discs
+    discs.forEach(({ manufacturer }) => {
+      manufacturers.add(manufacturer)
+    });
+
+    // Convert the processed data back to JSON
+    const newData = JSON.stringify([...manufacturers].sort(), null, 2);
+    
+    // Write the processed data to discs.json
+    await fs.writeFile('./data/manufacturers.json', newData, 'utf-8');
+    
+    console.log('Successfully processed discs and wrote to discs.json');
+  } catch (error) {
+    console.error('Error processing discs:', error);
+  }
+}
+
+getManufacturers();
