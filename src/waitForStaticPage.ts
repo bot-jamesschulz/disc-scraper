604510381de02d464a1ffb2c9b4c3f2afcaeb27b
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-07-20 20:48:17 -0700

    Added timeout

--- src/waitForStaticPage.ts
+++ src/waitForStaticPage.ts
@@ -1,31 +1,35 @@
 import waitForNewContent from './waitForNewContent';
 import scrollPage from './scrollPage';
 import { Page } from "puppeteer";
+import delay from '../utils/delay';
 
-export default async function waitForStaticPage(page: Page, maxAttempts = 5) {
+export default async function waitForStaticPage(page: Page, maxAttempts = 5, timeout=30000) {
+    await Promise.race([delay(timeout), wait()])
     
-    let attempts = 0;
-    let pageLoaded;
-  
-    while(!pageLoaded && attempts <= maxAttempts) {
-      attempts++;
-      console.log('attempts', attempts)
-      try { 
-        await waitForNewContent(page);
-        if (!page) continue;
-        pageLoaded = await page.evaluate(() => document.readyState === 'complete');
-        if (!pageLoaded) continue;
-        await scrollPage(page);
-        // wait for new lazy loaded content triggered by scrolling
-        await waitForNewContent(page, {minFulfilled: 0});
-      } catch(err) {
-        console.log('Error checking static state:', err)
-        pageLoaded = false;
+    async function wait() {
+      let attempts = 0;
+      let pageLoaded;
+
+      while(!pageLoaded && attempts <= maxAttempts) {
+        attempts++;
+        console.log('attempts', attempts)
+        try { 
+          await waitForNewContent(page);
+          if (!page) continue;
+          pageLoaded = await page.evaluate(() => document.readyState === 'complete');
+          if (!pageLoaded) continue;
+          await scrollPage(page);
+          // wait for new lazy loaded content triggered by scrolling
+          await waitForNewContent(page, {minFulfilled: 0});
+        } catch(err) {
+          console.log('Error checking static state:', err)
+          pageLoaded = false;
+        }
+    
+        if (attempts > maxAttempts) {    
+          throw new Error('Max attempts for network idle reached');
+        }  
       }
-  
-      if (attempts > maxAttempts) {    
-        throw new Error('Max attempts for network idle reached');
-      }  
     }
   
     console.log("Page is static");
