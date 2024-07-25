12188b70bb5986568cffbc4c5ed8dd2aa6fa7da1
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-07-24 22:48:06 -0700

    Initial retialer iteration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import navigateTo from "./navigateTo";
-import { type ListingData, ListingImgs, ListingPrices } from "./getListingData";
+import { type ListingData, ListingImgs, ListingPrices } from "./getPageData";
 import validateListings, { type ValidatedListingTitles } from '../utils/validateListings';
 import groupListingData, { type Listing } from "../utils/groupListingData";
 import "dotenv/config";
@@ -12,7 +12,8 @@ import searchInventory from "./searchInventory";
 const retailersJson = fs.readFileSync('./data/retailers.json', 'utf-8');
 const retailers = JSON.parse(retailersJson);
 const manufacturersJson = fs.readFileSync('./data/majorManufacturers.json', 'utf-8');
-const manufacturers = JSON.parse(manufacturersJson);
+// const manufacturers = JSON.parse(manufacturersJson);
+const manufacturers = ['Axiom'];
 
 export type ValidatedListing = {
   listings: ValidatedListingTitles,
@@ -23,68 +24,76 @@ export type ValidatedListing = {
 puppeteer.use(StealthPlugin());
 
 async function scrape() {
-  const browser = await puppeteer.launch({ headless: false });
-
-  const discStore = retailers[25];
-
-  const page = await browser.newPage();
-
+  
+  let browser;
+  let page;
   try {
-    
-    for (const manufacturer of manufacturers) {
-      await navigateTo(
-        discStore, 
-        page
-      );
-
-      await searchInventory(page, manufacturer);
-      
-      const listingData: ListingData[] = await extractInventory(page)
-
-      // Validate listings and group attributes
-      const validatedListingPages: ValidatedListing[] = [];
-      const listings: Listing[] = []
+    for (const retailer of retailers.slice(10,25)) {
+      browser = await puppeteer.launch({ headless: false });
+      page = await browser.newPage();
       try {
-        for (const dataPage of listingData) {
-        // Validate the anchors per page
-          const validationListings = validateListings(dataPage.listings, manufacturer, discStore)
-          // Add valid listings with their correlated prices and imgs
-          if (validationListings.size) {
-            validatedListingPages.push({
-              listings: validationListings,
-              prices: dataPage.listingPrices,
-              imgs: dataPage.listingImgs
-            })
-          }
-        }
-      } catch (err) {
-        console.log('Error validating listings', err);
-      }
+        for (const manufacturer of manufacturers) {
+          await navigateTo(
+            retailer, 
+            page
+          );
 
-        // associate listings with closest img and price
-        if (validatedListingPages.length) {
+          await searchInventory(page, manufacturer);
+          
+          const listingData: ListingData[] = await extractInventory(page)
 
-          validatedListingPages.forEach(pageListings =>  {
-            const groupedListings = groupListingData(pageListings)
-
-            if (groupedListings) {
-              listings.push(...groupedListings);
+          // Validate listings and group attributes
+          const validatedListingPages: ValidatedListing[] = [];
+          const listings: Listing[] = []
+          try {
+            for (const dataPage of listingData) {
+            // Validate the anchors per page
+              const validationListings = validateListings(dataPage.listings, manufacturer, retailer)
+              // Add valid listings with their correlated prices and imgs
+              if (validationListings.size) {
+                validatedListingPages.push({
+                  listings: validationListings,
+                  prices: dataPage.listingPrices,
+                  imgs: dataPage.listingImgs
+                })
+              }
             }
-          })
-        }
-      
-      // Convert the processed data back to JSON
-      const newData = JSON.stringify(listings, null, 2);
-        
-      // Write the processed data to discs.json
-      fs.writeFile(`./results/${manufacturer}Results.json`, newData, () => {});
-    }
+          } catch (err) {
+            console.log('Error validating listings', err);
+          }
 
-    await browser.close();
-    } catch(e) {
-      await browser.close();
-      console.log(e)
+            // associate listings with closest img and price
+            if (validatedListingPages.length) {
+
+              validatedListingPages.forEach(pageListings =>  {
+                const groupedListings = groupListingData(pageListings)
+
+                if (groupedListings) {
+                  listings.push(...groupedListings);
+                }
+              })
+            }
+          
+          // Convert the processed data back to JSON
+          const newData = JSON.stringify(listings, null, 2);
+          const url = new URL(retailer);  
+          // Write the processed data to discs.json
+          fs.mkdir(`./results/${url.hostname}`, () => {
+            fs.writeFile(`./results/${url.hostname}/${manufacturer}Results.json`, newData, () => {});
+          })
+          
+        }
+      } catch(e) {
+        console.log(e);
+      } finally {
+        await browser.close();
+      }
     }
+  } catch(e) {
+    console.log(e)
+  } finally {
+    await browser?.close();
+  }
 }
 try {
     scrape();
