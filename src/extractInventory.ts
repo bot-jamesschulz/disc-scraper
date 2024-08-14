40ee7c3f4b058f9493a32266126dc363cdb20d64
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-08-14 11:21:25 -0700

    Image association improvements

--- src/extractInventory.ts
+++ src/extractInventory.ts
@@ -1,13 +1,11 @@
-import getPageListings, { 
-  type ListingData,
-  type ListingPrices,
-  type ListingImgs
+import { 
+  type ListingPrice
 } from "./getPageListings";
-import extractImages from './extractImages';
-import waitForStaticPage from "./waitForStaticPage";
-import isNewListings from "../utils/isNewListings";
-import { paginationListings } from "./iterationMethods";
-import validateListings, {
+import { 
+  infiniteScrollListings,
+  paginationListings
+} from "./iterationMethods";
+import {
   type ValidatedListings,
   type Listing
 } from "../utils/validateListings";
@@ -15,55 +13,29 @@ import { type Page } from 'puppeteer';
 
 export type ValidatedListingsPage = {
   listings: ValidatedListings,
-  prices: ListingPrices,
+  prices: ListingPrice[],
   imgs: string[]
 }
 
 export default async function extractInventory(page: Page, manufacturer: string, retailer: string): Promise<Listing[]> {
-  const validatedHrefs = new Set<string>();
-  const listings: Listing[] = [];
-  // Keeps track of all the potential listings. This is important to keep track of as well as validated listings
-  // Becasue we use this to decide when to stop paginating. We don't want to stop just because one page doesn't have any valid listings.
-  const listingData: ListingData[] = [];
-  let newListingData;
-  let scrollIterations = 0;
 
-  // Get infinite loaded listings
-  do {
-    scrollIterations++;
-    const pageListings = await getPageListings(page);
-
-    if (pageListings) {
-      listingData.push(pageListings);
-      const validatedListings = await validateListings(page, pageListings, manufacturer, retailer);
-
-      if (validatedListings.length) {
-        listings.push(...validatedListings)
-        
-      }
+  try {
+    const { listings: validatedInfiniteScrollListings, isInfiniteScroll } = await infiniteScrollListings(page, manufacturer, retailer);
+    
+    if (isInfiniteScroll) {
+      return validatedInfiniteScrollListings;
     }
     
-    await waitForStaticPage(page);
-
-    newListingData = await getPageListings(page);
-  } while (isNewListings(listingData.map(data => data.listings).flat(), newListingData?.listings));
-  
-  // If there was infinite scroll then skip checking for pagination
-  if (scrollIterations > 1) {
-    console.log('Infinite scroll listings. Pagination not attempted');
-    return listings;
-  }
-  
-  let validatedListings
-  try {
-    validatedListings = await paginationListings(page, manufacturer, retailer);
   } catch(e) {
-    console.log('Error extracting paginated listings', e)
-  }
-  if (validatedListings) {
-    listings.push(...validatedListings);
+    console.log('Error extracting infinite scroll listings', e);
   }
 
 
-  return listings;
+  try {
+    return await paginationListings(page, manufacturer, retailer);
+  } catch(e) {
+    console.log('Error extracting paginated listings', e);
+  }
+
+  return [];
 }
--- src/getPageListings.ts
+++ src/getPageListings.ts
@@ -2,20 +2,30 @@ import delay from "../utils/delay";
 import { Page } from "puppeteer";
 
 export type ListingTitle = {
-  listingIndex: number;
-  innerText: string;
+  listingIndex: number,
+  position: Position,
+  innerText: string,
   href: string;
 };
-
-export type ListingImgs = {
-  [key: string]: string
+export type Position =  {
+  x: number,
+  y: number
+}
+export type ListingImg = {
+  src: string,
+  topCoord: number,
+  position: Position
 };
 
-export type ListingPrices = (string | null) [];
+export type ListingPrice = {
+  price: string,
+  position: Position
+};
 
 export type ListingData = {
     listings: ListingTitle[],
-    listingPrices: ListingPrices,
+    listingPrices: ListingPrice[],
+    listingImgs: ListingImg[]
     url: string
 };
 
@@ -52,21 +62,20 @@ async function extractData(page: Page): Promise<ListingData | undefined> {
         const maxTextLength = 250;
         let curImgWait = 0;
         const maxImgWait = 5000;
-        let prevImgIndex = 0;
         const listings: ListingTitle[] = [];
-        const listingImgs: ListingImgs = {};
-        const listingPrices: ListingPrices = [];
+        const listingImgs: ListingImg[] = [];
+        const listingPrices: ListingPrice[] = [];
         const elementNodes = document.querySelectorAll("*");
         const elements = Array.from(elementNodes) as HTMLElement[];
   
         for (const [index, element] of elements.entries()) {
           if (!element) continue;
-  
-          // Get text content and associated hrefs
-          const backgroundImg =
-            window.getComputedStyle(element).backgroundImage === "none"
-              ? null
-              : window.getComputedStyle(element).backgroundImage;
+
+          const rect = element.getBoundingClientRect();
+          const position = {
+            x: rect.left + rect.width / 2,
+            y: rect.top + rect.height / 2
+          }
   
           const trimmedText = element.innerText
             ?.replace(/\r?\n|\r|\s+/g, " ")                       // Replace newlines and consecutive spaces with a single space
@@ -88,19 +97,20 @@ async function extractData(page: Page): Promise<ListingData | undefined> {
           if (wholeText?.includes("$") && wholeText?.length < maxTextLength) {
             
             const priceRegex = /\$[\d.]+/;
-  
             let price = trimmedText?.match(priceRegex);
-
             let currElement = element;
+
             while (!price && currElement.parentNode) {
               currElement = currElement.parentNode as HTMLElement;
               price = currElement?.innerText?.match(priceRegex);
             }
   
             const trimmedPrice = price?.[0].replace(/[^\d.]/g, ""); // Remove everything but digits and "."
-            
             if (trimmedPrice) {
-              listingPrices[index] = trimmedPrice;
+              listingPrices.push({
+                price: trimmedPrice,
+                position,
+              });
             }
           }
   
@@ -108,24 +118,98 @@ async function extractData(page: Page): Promise<ListingData | undefined> {
           if (trimmedText && element.tagName === "A") {
             listings.push({
               listingIndex: index,
+              position,
               innerText: trimmedText,
               href: element.getAttribute("href") || "",
             });
           }
-  
-          // Make sure that the background-image isn't part of a subsection/gallery of images
-          if (backgroundImg) {
-            const backgroundImgUrlMatch = backgroundImg.match(/url\("(.+)"\)/); // Extract the url
-            const backgroundImgUrl = backgroundImgUrlMatch
-              ? backgroundImgUrlMatch[1]
-              : null;
-            if (!backgroundImgUrl || backgroundImgUrl.includes(".gif")) continue;
-            listingImgs[index] = backgroundImgUrl; // Save the img's url with an associated element index, for use later to find closest listing element
-            prevImgIndex = index;
+
+          if ((element.tagName === 'IMG') || (element.tagName === 'INPUT') 
+            && (element.hasAttribute('src') || element.hasAttribute('srcset'))) {
+              
+              const waitInterval = 100; // Time to wait before checking the src attribute again
+              const maxWaitTime = 500; // Maximum wait time for checking src
+              let elapsedTime = 0;
+              let waitCount = 0;
+      
+              // Wait for src attribute to be set
+              const waitForSrc = async () => {
+
+                if (element.getAttribute("srcset")) {
+                  const srcset = element.getAttribute("srcset");
+                  const sources = srcset?.split(',').map(item => {
+                    const [url, descriptor] = item.trim().split(' ');
+                    const width = parseInt(descriptor.replace('w', ''), 10);
+                    return { url, width };
+                  });
+
+                  // Sort the sources by pixel width
+                  sources?.sort((a, b) => a.width - b.width);
+
+                  let medianIndex;
+                  if (sources) medianIndex = Math.floor(sources.length / 2);
+                  let medianSrc;
+                  if (medianIndex) medianSrc = sources?.[medianIndex].url;
+
+                  if (medianSrc) listingImgs.push({ 
+                      src: medianSrc,
+                      topCoord: rect.top,
+                      position
+                  });
+                  return;
+                }
+                  
+                  // // Get first url of scrset
+                  // if (element.getAttribute("srcset")) {
+                  //     const url = element.getAttribute("srcset");
+                  //     const endOfUrl = url?.indexOf(" ");
+                  //     const firstUrl = endOfUrl !== -1 ? url?.substring(0, endOfUrl) : url;
+                  //     if (firstUrl) listingImgs.push({ 
+                  //         src: firstUrl,
+                  //         topCoord: rect.top,
+                  //         position
+                  //     });
+                  //     return;
+                  // }
+
+                  if (element.getAttribute("src")) {
+                      let url;
+                      try {
+                      const src = element.getAttribute("src");
+                      if (src) {
+                          url = new URL(src, window.location.href);
+                      }
+                      } catch (err) {}
+      
+                      if (url?.href.startsWith("http")) {
+                          listingImgs.push({ 
+                              src: url.href, 
+                              topCoord: rect.top,
+                              position
+                          });
+                          return;
+                      }
+                  }
+                  
+                  elapsedTime += waitInterval;
+                  curImgWait += waitInterval;
+                  ++waitCount;
+                  if (
+                      elapsedTime < maxWaitTime
+                      && curImgWait < maxImgWait
+                  ) {
+                      console.log('waiting')
+                      await new Promise((resolve) => setTimeout(resolve, waitInterval));
+                      await waitForSrc();
+                  }
+              };
+              await waitForSrc();     
+              
           }
-        }
   
-        return { listings, listingPrices, url: window.location.href };
+        } 
+  
+        return { listings, listingPrices, listingImgs, url: window.location.href };
       });
     } catch (err) {
       console.log("error retrieving data/images from the DOM", err);
@@ -134,4 +218,5 @@ async function extractData(page: Page): Promise<ListingData | undefined> {
   
     return listingData;
   }
+
   
